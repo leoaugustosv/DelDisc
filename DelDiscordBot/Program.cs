21fb14bfbe73fa59583bcf3e609ec406bf72fb04
leoaugustosv <kleeniietv@gmail.com>
2024-04-04 23:18:02 -0300

    feat: initial commit

---
+++ DelDiscordBot/Program.cs
@@ -0,0 +1,54 @@
+﻿using DelDiscordBot.config;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelDiscordBot
+{
+    internal class Program
+    {
+        //Getting the main discord settings, client and commands
+        private static DiscordClient Client { get; set; }
+        private static CommandsNextConfiguration Commands { get; set; }
+
+        //Our main will be a task that will loop every time it is connected to the token 
+
+        static async Task Main(string[] args)
+        {
+            try { 
+
+            var JsonReader = new JSONReader();
+            await JsonReader.ReadJSON();
+
+            var discordConfig = new DiscordConfiguration()
+            {
+                Intents = DiscordIntents.All,
+                Token = JsonReader.token,   
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+            };
+
+            //Instantiating a variable so that it has the new settings
+            Client = new DiscordClient(discordConfig);
+            Client.Ready += Client_Ready; //It's adding an event handler for the Discord client's Ready event.
+
+            await Client.ConnectAsync();
+            await Task.Delay(-1);
+            }
+            catch(Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static Task Client_Ready(DiscordClient sender, ReadyEventArgs args)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ DelDiscordBot/config/JSONReader.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelDiscordBot.config
+{
+    //For security reasons with the bot token, it is necessary to create a class to read it and a class to capture its JSON structure
+    internal class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+               string json = await sr.ReadToEndAsync();
+               JSONStructure data = JsonConvert.DeserializeObject <JSONStructure>(json);
+
+               this.token = data.token;
+               this.prefix = data.prefix;
+            }
+        }
+
+    }
+    internal sealed class JSONStructure
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
