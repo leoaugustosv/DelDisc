ac9f889eef101dfa6480bbf016805580f77369f7
leoaugustosv <kleeniietv@gmail.com>
2024-04-05 23:09:42 -0300

    fix: validate channel creation commands

--- DelDiscordBot/commands/TextCommands.cs
+++ DelDiscordBot/commands/TextCommands.cs
@@ -34,6 +34,7 @@ namespace DelDiscordBot.commands
 
             // Send a Confirmation!
             await ctx.RespondAsync("All messages in this channel have been cleaned.");
+
         }
 
 
@@ -41,6 +42,7 @@ namespace DelDiscordBot.commands
         [Description("Creates a new text channel within the server.")]
         public async Task NewTextChannel(CommandContext ctx, String channelname)
         {
+
             // Permissions
             if (!ctx.Member.PermissionsIn(ctx.Channel).HasPermission(Permissions.ManageChannels))
             {
@@ -55,8 +57,11 @@ namespace DelDiscordBot.commands
                 return;
             }
 
-            //To-do: Validating input!!!
-
+            if (channelname.Equals(""))
+            {
+                await ctx.RespondAsync("You forgot to specify a name for the new text channel!");
+                return;
+            }
             await ctx.Guild.CreateChannelAsync(channelname, ChannelType.Text);
 
 
@@ -66,7 +71,7 @@ namespace DelDiscordBot.commands
 
         [Command("V")]
         [Description("Creates a new voice channel within the server.")]
-        public async Task NewVoiceChannel(CommandContext ctx, String channelname)
+        public async Task NewVoiceChannel(CommandContext ctx, String channelname = "")
         {
 
                 // Permissions
@@ -76,17 +81,50 @@ namespace DelDiscordBot.commands
                     return;
                 }
 
-                //To-do: Validating input!!!
+                if (channelname.Equals("")){
+                    await ctx.RespondAsync("You forgot to specify a name for the new voice channel!");
+                    return;
+            }
+            await ctx.Guild.CreateChannelAsync(channelname, ChannelType.Voice);
+                
 
-
-                await ctx.Guild.CreateChannelAsync(channelname, ChannelType.Voice);
-
-
-                // Send a Confirmation!
-                await ctx.RespondAsync("Voice channel \"" + channelname + "\" created.");
+            // Send a Confirmation!
+            await ctx.RespondAsync("Voice channel \"" + channelname + "\" created.");
 
             
         }
+
+        [Command("Spam")]
+        [Description("Spawn a lot (a lot) of messages!")]
+        public async Task SpamMessages(CommandContext ctx, int quantity = 20)
+        {
+
+            // SPAMMMM!
+            for (int i = 1; i <= quantity; i++) { 
+            await ctx.RespondAsync("Message "+i+" :SPAMMMMMM MUAHAHAHAHAHA");
+            }
+
+        }
+
+        [Command("R")]
+        [Description("Delete and create a new channel with the same name, in the same category.")]
+        public async Task RecreateChannel(CommandContext ctx, int quantity = 20)
+        {
+
+            // TO-DO: Check if channel is in a category; if true, get category
+
+
+            // Delete channel
+            await ctx.Channel.DeleteAsync();
+
+
+            // TO-DO: Create a channel with the same name, in the same category
+
+        }
+
+
+
+
     }
 
 }
